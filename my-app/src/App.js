b0cb7875da60b766778333fe2d73a025c5c4aff3
Fatih Aytuğ Gör <fatihaytuggor@gmail.com>
2022-10-11 17:07:04 +0300

    Başlangıç(devam edilecek)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <h3>Test</h3>
+    <Sidebar />
   );
 }
 
---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+function Sidebar(props) {
+    return {
+        <div className="logo">
+           <a>Brand<b>Colors</b></a>
+        </div>
+
+    }
+}
+
+export default Sidebar;
