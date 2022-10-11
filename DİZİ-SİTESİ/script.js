529fe50727414cdda11084ba0294f7bd4ea5de4d
Fatih Aytuğ Gör <fatihaytuggor@gmail.com>
2022-10-11 14:14:38 +0300

    update

--- DİZİ-SİTESİ/script.js
+++ DİZİ-SİTESİ/script.js
@@ -4,6 +4,7 @@ const IMG_Path = 'https://image.tmdb.org/t/p/w1280';
 
 const Search_Url = 'https://api.themoviedb.org/3/search/movie?api_key=15005c48f2625def09dacd3d0fc01762&query="';
 
+const main=document.getElementById('main');
 const form=document.getElementById('form');
 const search=document.getElementById('search');
 
@@ -14,6 +15,34 @@ async function getMovies(url){
 }
 function showMovies(movies){
     main.innerHTML='';
+    movies.forEach((movie)=>{
+        const {title, poster_path, vote_average, overview}=movie;
+        const movie=document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML='
+        <div class="movie">
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            ${overview}
+        </div>
+    </div>
+    '
+
+    main.appendChild(movieEl);
+    });
+}
+function getClassByRate(vote) {
+    if (vote>=8){
+        return 'green'
+    }else if(vote>=5){
+        return 'orange'
+    }else{
+        return 'red';
+    }
 }
 
 form.addEventListener('submit', (e)=>{
